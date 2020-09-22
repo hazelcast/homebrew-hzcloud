cee74fef9aefd62ecc539a8ebbf701955377a877
Hüseyin BABAL <huseyinbabal88@gmail.com>
2020-09-22 16:19:47 +0300

    Create hzcloud.rb
    Homebrew formula initial version

---
+++ Formula/hzcloud.rb
@@ -0,0 +1,20 @@
+class Hzcloud < Formula
+  desc "Hazelcast Cloud Enterprise is an enterprise-grade, on-demand managed service for Hazelcast IMDG"
+  homepage "http://cloud.hazelcast.com/"
+  url "https://github.com/hazelcast/hazelcast-cloud-cli/archive/v0.0.0.tar.gz"
+  sha256 "034fbb014c97b706e9c42b737283b714e35736fe8ef43ed3c7a7a57fa09d2d5b"
+  depends_on "go" => :build
+  
+  def install
+      ENV["GOPATH"] = buildpath
+      bin_path = buildpath/"src/github.com/hazelcast/hazelcast-cloud-cli"
+      bin_path.install Dir["*"]
+      cd bin_path do
+        system "go", "build", "-ldflags", "-X github.com/hazelcast/hazelcast-cloud-cli/internal.Version=#{version} -X github.com/hazelcast/hazelcast-cloud-cli/internal.Distribution=BREW", "-o", bin/"hzcloud", "."
+      end
+  end
+
+  test do
+    system "false"
+  end
+end
